a637ee66f5ee6988e4b1bf94fc7f574a086bea37
darksider92 <darksider92@mail.ru>
2018-04-10 20:58:48 +0600

    v2

--- price_coin.py
+++ price_coin.py
@@ -6,38 +6,6 @@ URL = "https://api.coinmarketcap.com/v1/global/"
 tickerURL = "https://api.coinmarketcap.com/v1/ticker/"
 
 
-class Portfolio:
-    def __init__(self, ripple, litecoin, ethereum, bitcoin):
-        self.ripple = ripple
-        self.litecoin = litecoin
-        self.ethereum = ethereum
-        self.bitcoin = bitcoin
-
-    def add_coin(self, coin, value):
-        if coin == "ethereum":
-            self.ethereum += value
-        elif coin == "bitcoin":
-            self.bitcoin += value
-        elif coin == "litecoin":
-            self.litecoin += value
-        elif coin == "ripple":
-            self.ripple += value
-        else:
-            print("Такой валюты нет!")
-
-    def take_away_coin(self, coin, value):
-        if coin == "ethereum":
-            self.ethereum -= value
-        elif coin == "bitcoin":
-            self.bitcoin -= value
-        elif coin == "litecoin":
-            self.litecoin -= value
-        elif coin == "ripple":
-            self.ripple -= value
-        else:
-            print("Такой валюты нет!")
-
-
 def my_coin_porfolio():
     """
     Тут вводится информация по портфелю и сохроняется в json файл
@@ -76,22 +44,75 @@ def print_price(data, count_coins):
     print("Итого: " + str(float(price) * count_coins) + "$")
 
 
-def create_dict():
-    """
-    Создаем экземпляр класса
-    """
-    my_coins = Portfolio(result["ripple"], result["litecoin"], result["ethereum"], result["bitcoin"])
-    print(my_coins)
+def add_coin(coin, number):
+    totals = {}
+    with open("db.json", "r", encoding="UTF-8") as f:
+        date = json.load(f)
+        for k, v in date.items():
+            if coin == k:
+                summ = v + number
+                totals[k] = summ
+            elif coin != k:
+                totals[k] = v
+
+    with open("db.json", "w", encoding="UTF-8") as file:
+        json.dump(totals, file)
+
+
+def take_away(coin, number):
+    totals = {}
+    with open("db.json", "r", encoding="UTF-8") as f:
+        date = json.load(f)
+        for k, v in date.items():
+            if coin == k:
+                summ = v - number
+                totals[k] = summ
+            elif coin != k:
+                totals[k] = v
+
+    with open("db.json", "w", encoding="UTF-8") as file:
+        json.dump(totals, file)
 
 
 if __name__ == "__main__":
-    if os.path.isfile("E:\\Python\\coin\\db.json"):
-        pass
-    else:
-        my_coin_porfolio()
-    with open("db.json", "r", encoding="UTF-8") as f:
-        result = json.load(f)
-        create_dict()
-        for key, value in result.items():
-            param = input_coin(key)
-            print_price(param, value)
+    while True:
+        print("""
+            n - создание базы данных
+            d - удаление базы данных
+            c - получение информации по криптовалюте
+            a - добавление криптовалюты в портфель
+            t - отнимаем криптовалюту с портфеля
+            q - выход
+        """)
+        name_inp = input("Введите ключ: ")
+        if name_inp == "n":
+            if os.path.isfile(os.getcwd() + "db.json"):
+                pass
+            else:
+                my_coin_porfolio()
+        if name_inp == "c":
+            with open("db.json", "r", encoding="UTF-8") as f:
+                result = json.load(f)
+                for key, value in result.items():
+                    param = input_coin(key)
+                    print_price(param, value)
+        if name_inp == "a":
+            print("ripple", " bitcoin", " litecoin", " ethereum")
+            coin_name = input("Введите имя криптовалюты: ").lower()
+            coin_value = float((input("Введите число для добавления: ")))
+            add_coin(coin_name, coin_value)
+        if name_inp == "t":
+            print("ripple", " bitcoin", " litecoin", " ethereum")
+            coin_name = input("Введите имя криптовалюты: ").lower()
+            coin_value = float((input("Введите число для вычитание: ")))
+            take_away(coin_name, coin_value)
+        if name_inp == "q":
+            break
+        if name_inp == "d":
+            print("Вы точно хотите сбросить базу данных? ")
+            print("Yes, No")
+            answer = input(": ")
+            if answer == "Yes".lower():
+                os.remove("db.json")
+            elif answer == "No".lower():
+                pass
